0a21ae366459f131e59eff998e0ccc3678f08862
deliacirstea <deliutacirstea@gmail.com>
2022-10-27 09:07:40 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿//Solution found by my teacher
+
+
+
+
+
+
+try
+{
+    int targetNumber = new Random().Next(10);
+    List<int> previousGuesses = new List<int>();
+
+    while (true)
+    {
+        int number;
+        bool guessed;
+        do
+        {
+            Console.WriteLine("Pick a number between 0 and 9 (inclusive): ");
+            number = Convert.ToInt32(Console.ReadLine());
+            guessed = previousGuesses.Contains(number);
+
+            if (guessed)
+                Console.WriteLine("That number has been guessed before:");
+        }
+        while (guessed);
+
+        if (number == targetNumber) throw new Exception();
+        previousGuesses.Add(number);
+    }
+    
+}
+            catch (Exception)
+        {
+            Console.WriteLine("That was a bad number! You lose! Muhahahhahahaha!");
+        }
